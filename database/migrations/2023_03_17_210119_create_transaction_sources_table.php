f9100ac2a077839adb1a6d8db1dc4dd30bd77047
Alifkm <alifkahfi@gmail.com>
2023-03-21 02:34:10 +0700

    fix: change table

--- database/migrations/2023_03_17_210119_create_transaction_sources_table.php
+++ database/migrations/2023_03_17_210119_create_transaction_sources_table.php
@@ -15,16 +15,9 @@ return new class extends Migration
     {
         Schema::create('transaction_sources', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('transaction_type_id')->unsigned();
             $table->integer('profit_loss_component_id')->unsigned();
             $table->string('transaction_source_name');
             $table->timestamps();
-        });
-
-        Schema::table('transaction_sources', function (Blueprint $table) {
-            $table->foreign('transaction_type_id')
-                ->references('id')
-                ->on('transaction_types');
         });
 
         Schema::table('transaction_sources', function (Blueprint $table) {
